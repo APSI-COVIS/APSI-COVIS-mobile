351aea083088d4574b5fad1711052fb6499bca2e
Marcel <jezierskimarcel@gmail.com>
2020-04-27 00:03:52 +0200

    basic model prepared

--- app/src/main/java/com/main/covis/covid_plot/CovidPlotModel.java
+++ app/src/main/java/com/main/covis/covid_plot/CovidPlotModel.java
@@ -1,6 +1,24 @@
 package com.main.covis.covid_plot;
 
+import java.util.Date;
+import java.util.List;
+
 public class CovidPlotModel {
+
+    public class CovidData{
+        private List<Country> countries;
+    }
+    public class Country{
+        private String name;
+        private String code;
+        List<CovidDateData> covidDatesData;
+    }
+    public class CovidDateData{
+        private Date date;
+        private Integer infectedNr;
+        private Integer curedNr;
+        private Integer deadNr;
+    }
 
     private String msg;
     private String msg2;
